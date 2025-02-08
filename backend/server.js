c5bfc1f070356dcd91b61b10386d475b584cd5b2
sunny kavar <kavarsunny51@gmail.com>
2025-02-08 13:00:49 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(` Server is running on port:${PORT}`);
+});
+
+
+server.on("error", (err) => {
+  console.error(" Server Error for:", err.message);
+});
